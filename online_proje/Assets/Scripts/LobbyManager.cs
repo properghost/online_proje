0d23b663134dc65ed2750c79937f1bf2ee3ebfb3
properghost <bloodhail13@gmail.com>
2024-08-19 18:47:08 +0300

    Matchmaking Basics and UI

--- online_proje/Assets/Scripts/LobbyManager.cs
+++ online_proje/Assets/Scripts/LobbyManager.cs
@@ -1,13 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
+using Photon.Realtime;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class LobbyManager : MonoBehaviour
+public class LobbyManager : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
+    
+    public static LobbyManager instance;
+
+    public static LobbyManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<LobbyManager>();
+            }
+            return instance;
+        }
+    }
     void Start()
     {
+        Debug.Log("Connecting to master.");
+
+        
         PhotonNetwork.ConnectUsingSettings();
     }
+    void Update()
+    {
+        
+    }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Joined to master.");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joined to lobby.");
+    }
+
+    public void Matchmaking()
+    {
+        PhotonNetwork.JoinRandomOrCreateRoom();
+
+    }
+    
+
+    public override void OnJoinedRoom()
+    {
+        
+    }
+
+
 }
--- online_proje/Assets/Scripts/UIManager.cs
+++ online_proje/Assets/Scripts/UIManager.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class UIManager : MonoBehaviour
+public class UIManager : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject loadingPanel;
+    [SerializeField] private GameObject joinedLobbyText;
+    [SerializeField] private GameObject readyToPlayText;
     void Start()
     {
-        
+        loadingPanel.SetActive(true);
+        joinedLobbyText.SetActive(false);
+        readyToPlayText.SetActive(false);
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if(PhotonNetwork.CurrentRoom != null)
+        {
+            if(PhotonNetwork.CurrentRoom.PlayerCount == 2)
+            {
+                joinedLobbyText.SetActive(false);
+                readyToPlayText.SetActive(true);
+            }
+            else
+            {
+                joinedLobbyText.SetActive(true);
+                readyToPlayText.SetActive(false);
+            }
+        }
     }
+
+    public override void OnJoinedLobby()
+    {
+        loadingPanel.SetActive(false);     
+    }
+
+    public void ButtonForMatchmaking()
+    {
+        LobbyManager.Instance.Matchmaking();
+    }
+    public void QuitGame()
+    {
+        PhotonNetwork.LeaveRoom();
+        PhotonNetwork.LeaveLobby();
+        Application.Quit();
+    }
+
+    
 }
